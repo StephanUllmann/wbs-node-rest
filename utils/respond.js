a6456243d4b0dd8c5ee6ff9b265aebbf06f8d1a0
Stephan Ullmann <stephan.ullmann@posteo.de>
2025-01-17 15:15:01 +0100

    update respond function to handle non-JSON content types

--- utils/respond.js
+++ utils/respond.js
@@ -1,5 +1,6 @@
 export const respond = (res, statusCode, contentType, data) => {
   res.statusCode = statusCode;
   res.setHeader('Content-Type', contentType);
-  res.end(JSON.stringify(data));
+  if (contentType === 'application/json') res.end(JSON.stringify(data));
+  else res.end(data);
 };
